68b8cb9bf9108dfea514701f72c5fbd804bc0b74
Rafael Batista <maranata_rafa@hotmail.com>
2022-08-14 16:14:44 -0400

    CI: change project

--- FC.CI.App/TesteDiretorio/Teste.cs
+++
@@ -1,9 +0,0 @@
-namespace FC.CI.App.TesteDiretorio
-{
-    public class Teste
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Old { get; set; }
-    }
-}
