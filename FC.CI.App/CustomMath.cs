da46592addc6af2973f519128e7210046aec6c00
Rafael Batista <maranata_rafa@hotmail.com>
2022-08-15 02:29:31 -0400

    FEAT: add method math

--- FC.CI.App/CustomMath.cs
+++ FC.CI.App/CustomMath.cs
@@ -2,8 +2,16 @@ namespace FC.CI.App
 {
     public static class CustomMath
     {
-        //Somar
         public static int Somar(int n1, int nr2)
             => n1 + nr2;
+        
+        public static int Subtrair(int n1, int nr2)
+            => n1 - nr2;
+        
+        public static int Multiplicar(int n1, int nr2)
+            => n1 * nr2;
+        
+        public static int Dividir(int n1, int nr2)
+            => n1 / nr2;
     }
 }
--- FC.CI.Tests/ProgramTests.cs
+++ FC.CI.Tests/ProgramTests.cs
@@ -2,7 +2,7 @@ using FC.CI.App;
 
 namespace FC.CI.Tests
 {
-    public class UnitTest1
+    public class CustomMathTests
     {
         [Fact]
         public void GivenSum_WhenPassingIntParameters_ThenEnsureReturnSumFromParameters()
@@ -10,6 +10,30 @@ namespace FC.CI.Tests
             var result = CustomMath.Somar(10, 10);
             
             Assert.Equal(20, result);
+        }
+        
+        [Fact]
+        public void GivenSub_WhenPassingIntParameters_ThenEnsureReturnSumFromParameters()
+        {
+            var result = CustomMath.Subtrair(20, 10);
+            
+            Assert.Equal(10, result);
+        }
+        
+        [Fact]
+        public void GivenSDMult_WhenPassingIntParameters_ThenEnsureReturnSumFromParameters()
+        {
+            var result = CustomMath.Multiplicar(20, 10);
+            
+            Assert.Equal(200, result);
+        }
+        
+        [Fact]
+        public void GivenSDiv_WhenPassingIntParameters_ThenEnsureReturnSumFromParameters()
+        {
+            var result = CustomMath.Dividir(20, 10);
+            
+            Assert.Equal(2, result);
         }
     }
 }
